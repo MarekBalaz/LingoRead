9936cfbf9ac3b46163ef37a238e171d4af60d22a
Marek Balaz <marekgamingacc@gmail.com>
2024-09-02 21:01:25 +0200

    create skeleton for home and translator

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { TranslatorComponent } from './translator/translator.component';
+import { TabsComponent } from './tabs/tabs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: TabsComponent,
+        children: [
+            {
+                component: HomeComponent,
+                path: "home"
+            },
+            {
+                component: AccountComponent,
+                path: "account"
+            },
+            {
+                component: TranslatorComponent,
+                path: "translator"
+            },
+            {
+                component: HomeComponent,
+                path: "**"
+            }
+        ] 
+    }
+];
